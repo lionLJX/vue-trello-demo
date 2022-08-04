18e59761dc5c1d925b8bee512f026b697a497205
LionLJX <3435574021@qq.com>
2022-08-05 00:53:03 +0800

    perf:优化删除等

--- src/components/Home/coms/Contain/coms/Item.vue
+++ src/components/Home/coms/Contain/coms/Item.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="Item" v-if="isshow">{{todo.content}}
-    <div class="cha" v-on:click="chaShow">×</div>
+    <div class="choose" @click="chooseShow">...
+        <div class="cha" v-on:click="chaShow" v-show="whethershow">×</div>
+        <div class="move" v-show="moveshow">移动</div>
+    </div>
     </div>
 </template>
 
@@ -12,12 +15,18 @@ export default {
     },
     data() {
         return {
-            isshow : 'true'
+            isshow : true,
+            whethershow : false,
+            moveshow : false
         }
     },
     methods : {
         chaShow (){
             this.isshow = !this.isshow;
+        },
+        chooseShow () {
+            this.whethershow = !this.whethershow;
+            this.moveshow = !this.moveshow;
         }
     }
 }
@@ -25,13 +34,46 @@ export default {
 
 <style>
 .Item {
+    position: relative;
     color: #000;
-    display: inline-block;
+    display: block;
 }
 
 .cha {
+    position: absolute;
+    left: 20px;
+    top: 42px;
+    cursor: pointer;
+    width: 60px;
+}
+
+.choose {
     display: inline-block;
-    position: relative;
-    left: 105px;
+    position: absolute;
+    top: 0;
+    left: 290px;
+    cursor: pointer;
+}
+
+.cha,
+.move {
+    color: white;
+    background-color: rgb(1, 19, 31);
+    opacity: .7;
+}
+
+.move {
+    position: absolute;
+    top: 0;
+    left: 20px;
+    font-size: 16px;
+    font-weight: 400;
+    width: 60px;
+}
+
+.cha:hover,
+.move:hover {
+    left: 30px;
+    transition: .7s ease-in-out;
 }
 </style>
